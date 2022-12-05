f8d1fe19f7c266efbf6bfa98bf6102863dbe3344
kayce <justinross28@gmail.com>
2022-12-05 10:07:50 -0800

    controllers function

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -40,3 +40,51 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   }}
+  // update a thought
+  updateThought(req, res); {
+    Thoughts.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true })
+      .populate({ path: "reactions", select: "-__v" })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  }
+  // Delete a thought and associated apps
+  deleteThought(req, res); {
+    Thoughts.findByIdAndDelete(req.params.thoughtId)
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : Thoughts.deleteMany({ _id: { $in: thought.reactionId } })
+      )
+      .then(() =>
+        res.json({ message: "thought and associated reactions deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  }
+  // add a reaction
+  addReaction(req, res); {
+    Thoughts.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $addToSet: { reactions: req.body } },
+      { new: true }
+    )
+      .then((thoughtData) => {
+        !thoughtData
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thoughtData);
+      })
+      .catch((err) => res.status(500).json(err));
+  }
+  // delete a reaction
+  deleteReaction(req, res); {
+    Thoughts.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $pull: { reactions: {_id : req.params.reactionId }} },
+      { new: true }
+    )
+      .then((thoughtData) => {
+        !thoughtData
+          ? res.status(404).json({ message: "No reaction with that ID" })
+          : res.json(thoughtData);
+      })
+      .catch((err) => res.status(500).json(err));
+  }
