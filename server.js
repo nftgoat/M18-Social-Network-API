a2fa958ab71ee4aa15df4d178e987523114fd408
kayce <justinross28@gmail.com>
2022-12-05 06:27:40 -0800

    server changes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const db = require('./config/connection');
+const routes = require("./routes");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
